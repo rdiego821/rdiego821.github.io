017e369d939de3047db5f1c81d6ed1259a5291ad
rdiego821 <40282850+rdiego821@users.noreply.github.com>
2020-02-12 11:26:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 {
     "github_username": "rdiego821",
-    "github_token": "generated-token-like-abcder123456",
-    "github_webhook_path": "/github-webhook",
-    "github_webhook_secret": "shared-git-webhook-secret",
+    "github_token": "5a02532c09eb1d82f4df4b32f320b03af76828b8",
+    "github_webhook_path": "https://github.com/rdiego821/rdiego821.github.io/settings/hooks/183029497",
+    "github_webhook_secret": "LARGE_SECRET_PASSPHRASE",
     "delete_after_merge": false,
     "port": 
 }
