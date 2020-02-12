f20d733cecfb7007841e8273144d8047a1c7aabe
rdiego821 <40282850+rdiego821@users.noreply.github.com>
2020-02-12 11:42:26 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 {
     "github_username": "rdiego821",
     "github_token": "5a02532c09eb1d82f4df4b32f320b03af76828b8",
-    "github_webhook_path": "https://github.com/rdiego821/rdiego821.github.io/settings/hooks/183029497",
+    "github_webhook_path": "https://github.com/rdiego821/TestAtmerge/settings/hooks/183031663",
     "github_webhook_secret": "LARGE_SECRET_PASSPHRASE",
     "delete_after_merge": false,
     "port": 
